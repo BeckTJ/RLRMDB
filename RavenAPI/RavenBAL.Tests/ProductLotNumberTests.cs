f2d79cf24ed6185fd3a65c9157943aa9b130c944
BeckTJ <t.beck1@outlook.com>
2023-11-02 14:21:34 -0700

    Gotta figure out how to fack a db call

--- RavenAPI/RavenBAL.Tests/ProductLotNumberTests.cs
+++ RavenAPI/RavenBAL.Tests/ProductLotNumberTests.cs
@@ -1,4 +1,6 @@
 
+using RavenDAL.DTO;
+
 namespace RavenBAL.Tests
 {
     public class ProductLotNumberTests
@@ -19,24 +21,36 @@ namespace RavenBAL.Tests
             new RawMaterial{ProductId = "100SA", MaterialNumber = 123456, VendorLotNumber = "999-999-999"},
             new RawMaterial{ProductId = "101SA", MaterialNumber = 123456, VendorLotNumber = "999-999-999"},
         };
+        private readonly CreateRawMaterialDTO _createRawMaterialDTO = new CreateRawMaterialDTO
+        {
+            MaterialNumber = 123456,
+            VendorLotNumber = "999-999-999",
+            SampleId = "Raw45678",
+        };
 
         public ProductLotNumberTests()
         {
-            _pln = new ProductLotNumber();   
+            _pln = new ProductLotNumber(_repo);   
         }
 
         [Fact]
-        public void getInitProductLotNumber()
+        public void getNextProductLotNumber()
         {
             //Arrange
             var materialNumber = 123456;
+            var product = _material.SequenceId + _material.MaterialCode;
+            
+            //_repo.RawMaterial.GetRawMaterialByMaterialNumber(materialNumber).Returns(_rawMaterial);
+            //_repo.MaterialVendor.GetMaterialVendor(materialNumber).Returns(_material);
 
+            _pln.CreateProductLotNumber(_material.MaterialNumber).Returns(product);
 
             //Act
             var productId = _pln.CreateProductLotNumber(materialNumber);
 
 
             //Assert
+            Assert.Equal(product, productId);
 
 
         }
--- RavenAPI/RavenBAL/Services/ProductLotNumber.cs
+++ RavenAPI/RavenBAL/Services/ProductLotNumber.cs
@@ -1,4 +1,5 @@
-﻿using RavenBAL.Interface;
+﻿using Contracts;
+using RavenBAL.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,35 @@ namespace RavenBAL.Services
 {
     public class ProductLotNumber : IProductLotNumber
     {
+        private IRepoWrapper _repo;
+
+        public ProductLotNumber(IRepoWrapper repo) 
+        {
+            _repo = repo;
+        }
+
         public string CreateProductLotNumber(int materialNumber)
         {
-            throw new NotImplementedException();
+            var material = _repo.MaterialVendor.GetMaterialVendor(materialNumber);
+            var rawMaterial = _repo.RawMaterial.GetRawMaterialByMaterialNumber(materialNumber).OrderByDescending(rm => rm.ProductId).FirstOrDefault();
+            int id;
+            if (rawMaterial != null)
+            {
+                if (rawMaterial.ProductId.Length == 10 || rawMaterial.ProductId.Length == 6)
+                {
+                    id = int.Parse(rawMaterial.ProductId[..3]) + 1;
+                }
+                else
+                {
+                    id = int.Parse(rawMaterial.ProductId[..2]) + 1;
+                }
+                return id + material.MaterialCode;
+            }
+            else
+            { 
+                var productId = material.SequenceId + material.MaterialCode;
+                return productId;
+            }
         }
 
         public string UpdateProductLotNumber(string lotNumber)
